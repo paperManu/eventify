856852ab409450a8015013798a0274535163eaec
tart2000 <tart2000@gmail.com>
2014-06-04 16:26:16 -0400

    First commit

---
+++ karma.conf.js
@@ -0,0 +1,17 @@
+ files: [
+  'app/bower_components/jquery/jquery.js',
+  'app/bower_components/jquery-ui/ui/jquery-ui.js',
+  'app/bower_components/angular/angular.js',
+  'app/bower_components/angular-ui-sortable/sortable.js',
+  'app/bower_components/angular-mocks/angular-mocks.js',
+  'app/bower_components/angular-local-storage/angular-local-storage.js',
+  'app/scripts/*.js',
+  'app/scripts/**/*.js',
+  'test/mock/**/*.js',
+  'test/spec/**/*.js',
+  'app/bower_components/angular-resource/angular-resource.js',
+  'app/bower_components/angular-cookies/angular-cookies.js',
+  'app/bower_components/angular-sanitize/angular-sanitize.js',
+  'app/bower_components/angular-route/angular-route.js'
+]
+
